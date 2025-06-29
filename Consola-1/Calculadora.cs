a8cae55f036a1d6e682ed8a5a4ddcf764f403dff
ivanncomit <fabrivancj@gmail.com>
2025-06-29 19:28:10 -0300

    consola 1 terminada

--- Consola-1/Calculadora.cs
+++ Consola-1/Calculadora.cs
@@ -15,26 +15,26 @@ public class Calculadora
         public void Sumar(double termino)
     {
         dato = termino + dato;
-        Console.WriteLine(dato);
     }
     public void Restar(double termino)
     {
         dato = dato - termino;
-        Console.WriteLine(dato);
     }
     public void Multiplicar(double termino)
     {
         dato = termino * dato;
-        Console.WriteLine(dato);
     }
     public void Dividir(double termino)
     {
         dato = dato/termino;
-        Console.WriteLine(dato);
     }
     public void Limpiar()
     {
         Console.Clear();
+    }
+    public double Resultado
+    {
+        get => dato;
     }
 }
 
--- Consola-1/Program.cs
+++ Consola-1/Program.cs
@@ -2,16 +2,78 @@
 
 using EspacioCalculadora;
 
-Console.WriteLine("Hola mundo");
+string entrada;
+double dato, ingreso;
+int opcion = 1;
 
-//double resultado = EspacioCalculadora.Calculadora;
 Calculadora Resultado1 = new Calculadora(10);
 Calculadora Resultado = new Calculadora();
 
 Resultado.Limpiar();
+
 Console.WriteLine("------------------------");
+
 Resultado.Sumar(5);
 Resultado.Multiplicar(2);
+
 Console.WriteLine("------------------------");
+
 Resultado1.Dividir(5);
 Resultado1.Sumar(5);
+
+System.Console.WriteLine(Resultado.Resultado);
+
+do
+{
+    System.Console.WriteLine("A que valor desea aplicarle operaciones: ");
+    entrada = Console.ReadLine();
+    double.TryParse(entrada, out dato);
+} while (!double.TryParse(entrada, out dato));
+
+
+Calculadora calculos = new Calculadora(dato);
+
+do
+{
+    System.Console.WriteLine("Ingrese que operacion desea Realizar:\nSuma - 1\nResta - 2\nMultiplicacion - 3\nDivision - 4\nSALIR -c/o\n");
+    int.TryParse(Console.ReadLine(), out opcion);
+
+    switch (opcion)
+    {
+        case 1:
+            System.Console.WriteLine("\nCon que valor va a operar: ");
+            entrada = Console.ReadLine();
+            double.TryParse(entrada, out ingreso);
+            calculos.Sumar(ingreso);
+            System.Console.WriteLine("El resultado es:" + calculos.Resultado);
+            break;
+        case 2:
+            System.Console.WriteLine("\nCon que valor va a operar: ");
+            entrada = Console.ReadLine();
+            double.TryParse(entrada, out ingreso);
+            calculos.Restar(ingreso);
+            System.Console.WriteLine("El resultado es: " + calculos.Resultado);
+            break;
+        case 3:
+            System.Console.WriteLine("\nCon que valor va a operar: ");
+            entrada = Console.ReadLine();
+            double.TryParse(entrada, out ingreso);
+            calculos.Multiplicar(ingreso);
+            System.Console.WriteLine("El resultado es: " + calculos.Resultado);
+            break;
+        case 4:
+            do
+            {
+                System.Console.WriteLine("\nCon que valor va a operar: ");
+                entrada = Console.ReadLine();
+                double.TryParse(entrada, out ingreso);
+            } while (ingreso == 0);
+
+            calculos.Dividir(ingreso);
+            System.Console.WriteLine("El resultado es: " + calculos.Resultado);
+            break;
+    }
+
+
+
+} while (opcion > 0 && opcion < 5);
