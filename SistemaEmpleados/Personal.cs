bec26e38ed6fd5b0961574dc228671658a418703
ivanncomit <fabrivancj@gmail.com>
2025-06-29 21:43:28 -0300

    TP 7 terminado

--- SistemaEmpleados/Personal.cs
+++ SistemaEmpleados/Personal.cs
@@ -1,7 +1,5 @@
 namespace EspacioPersonal
 {
-    
-    // Enumeración para los cargos
     public enum Cargos
     {
         Auxiliar,
@@ -10,18 +8,109 @@ namespace EspacioPersonal
         Especialista,
         Investigador
     }
-
-    // Clase Empleado
     public class Empleado
     {
+        public Empleado(string nombreIngreso, string ApellidoIngreso, char estadoCivilIn, Cargos cargoingreso,
+        DateTime fechanac, DateTime fechaing, double sueldo)
+        {
+            this.apellido = ApellidoIngreso;
+            this.nombre = nombreIngreso;
+            this.estadoCivil = estadoCivilIn;
+            this.cargo = cargoingreso;
+            this.fechaNacimiento = fechanac;
+            this.FechaIngreso = fechaing;
+            this.SueldoBasico = sueldo;
+        }
+
         // Propiedades
 
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public DateTime FechaNacimiento { get; set; }
-        public char EstadoCivil { get; set; }
-        public DateTime FechaIngreso { get; set; }
+        private string nombre;
+        private string apellido;
+        private DateTime fechaNacimiento;
+        public char estadoCivil { get; set; }
+        private DateTime FechaIngreso;
         public double SueldoBasico { get; set; }
-        public Cargos Cargo { get; set; }
+        public Cargos cargo { get; set; }
+
+        public string NombreEmpleado
+        {
+            get => nombre;
+        }
+        public string ApellidoEmpleado
+        {
+            get => apellido;
+        }
+        public DateTime FNEmpleado
+        {
+            get => fechaNacimiento;
+        }
+        public DateTime FIEmpleado
+        {
+            get => FechaIngreso;
+        }
+
+
+        public int Antiguedad()
+        {
+            DateTime Actual = new DateTime();
+            Actual = DateTime.Now;
+
+            int antiguedad = Actual.Year - FechaIngreso.Year;
+            return antiguedad;
+        }
+        public int Edad()
+        {
+            DateTime Actual = new DateTime();
+            Actual = DateTime.Now;
+
+            int edad = Actual.Year - fechaNacimiento.Year;
+            return edad;
+        }
+        public int AniosJubilarse()
+        {
+            int edad = 65 - Edad();
+            if (edad <= 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return edad;
+            }
+        }
+        public double Salario()
+        {
+            double calculo = SueldoBasico + Adicional();
+            return calculo;
+        }
+
+        public double Adicional()
+        {
+            double multiplicador = 0;
+            double adicional = SueldoBasico;
+            if (Antiguedad() <= 20)
+            {
+                multiplicador = 0.01 * Antiguedad();
+            }
+            else
+            {
+                multiplicador = 0.25;
+            }
+
+            adicional = adicional * multiplicador;
+
+            if (cargo == Cargos.Ingeniero || cargo == Cargos.Especialista)
+            {
+                multiplicador = 0.50;
+                adicional = adicional + adicional * multiplicador;
+            }
+
+            if (estadoCivil == 'c')
+            {
+                adicional = adicional + 150000;
+            }
+
+            return adicional;
+        }
     }
 }
--- SistemaEmpleados/Program.cs
+++ SistemaEmpleados/Program.cs
@@ -1,6 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EspacioPersonal;
+double suma=0;
+int menor = 999;
 
-Console.WriteLine("Hello, World!");
+Empleado[] Empleados =
+{
+new Empleado("Pepe", "Argento", 'c', Cargos.Administrativo, new DateTime(1969,10,20), new DateTime (1999, 10, 11), 1000000)
+,
+new Empleado("Jorge", "Rodriguez", 's',Cargos.Auxiliar,new DateTime(2003,8,5), new DateTime(2025,1,1), 300000 )
+,
+new Empleado("Hermenegildo", "Gonzalez", 'v', Cargos.Ingeniero, new DateTime(1990, 10,2), new DateTime(2004,1,1), 800000)
+};
 
+foreach (var empleado in Empleados)
+{
+    System.Console.WriteLine($"Salario de {empleado.NombreEmpleado} es : {empleado.Salario()}");
+    suma += empleado.Salario();
+}
 
+System.Console.WriteLine("El monto total de salarios es: " + suma);
+
+foreach (var empleados in Empleados)
+{
+    if (menor > empleados.AniosJubilarse())
+    {
+        menor = empleados.AniosJubilarse();
+    }
+}
+
+foreach (var empleados in Empleados)
+{
+    if (empleados.AniosJubilarse() == menor)
+    {
+    System.Console.WriteLine($"Nombre: {empleados.NombreEmpleado}\nApellido: {empleados.ApellidoEmpleado}\nEdad: {empleados.Edad()}\nAntiguedad: {empleados.Antiguedad()}\nAños para Jubilarse: {empleados.AniosJubilarse()}\nSalario: {empleados.Salario()}");  
+    }
+}
